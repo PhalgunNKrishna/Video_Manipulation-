d0c17355435507ddeacb0cfe449a2a0315a09c1e
chaare24 <38058323+chaare24@users.noreply.github.com>
2019-08-05 16:32:26 -0700

    Input/Output working

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+
+public class Main
+{
+	public static void main(String args[]) {
+        VideoFileIO app = new VideoFileIO();
+        app.InOut();
+	}
+}
